0879bef948c3e9aca656fa37e4f6cbfc8b3f7f8e
Harshvardhan1407 <sharmaharshvardhan1407@gmail.com>
2024-08-16 18:32:32 +0530

    add file handling

--- pipeline/data_ingestion.py
+++ pipeline/data_ingestion.py
@@ -1,14 +1,28 @@
 import pandas as pd
+import os
 from logger import logger
 from config import data_path
+
 class DataIngestion:
     def initiateDataIngestion(self):
         try:
-            data = pd.read_csv(data_path,index_col=0) 
-            # logger.info("data read")
-            logger.info(f"----------data read".ljust(60, '-'))
+            file_name, file_extension = os.path.splitext(data_path)
+            if file_extension == ".csv":
+                if file_name == r"Datasets\flight_data":
+                    data = pd.read_csv(data_path,index_col=0) 
+                else:
+                    data = pd.read_csv(data_path)
 
+            if file_extension == ".parquet":
+                data = pd.read_parquet(data_path)
+            
+            if file_extension == ".json":
+                data = pd.read_json(data_path)
+
+            logger.info(f"----------data read".ljust(60, '-'))
+            logger.info(f"----------columns:{tuple(data.columns)}".ljust(60, '-'))
+            logger.info(f"----------shape:{data.shape}".ljust(60, '-'))
             return data
         
         except Exception as e:
-            logger.info(f"Error in Ingestion Process: {e}")
+            logger.error(f"Error in Ingestion Process: {e}",exc_info=True)
