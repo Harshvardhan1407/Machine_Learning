472cfe686e69f366b009a43f2ad49af6f757c861
Harshvardhan1407 <sharmaharshvardhan1407@gmail.com>
2024-08-22 18:34:41 +0530

    weather_data integration

--- pipeline/data_transformation.py
+++ pipeline/data_transformation.py
@@ -12,7 +12,7 @@ try:
     import holidays
 
     from sklearn.cluster import DBSCAN
-    from common import add_lags, create_features, get_database, holidays_list
+    from common import add_lags, create_features, get_database, holidays_list, label_encoding_decoding, data_validation
     import matplotlib.pyplot as plt
     from sklearn.model_selection import train_test_split
     from sklearn.model_selection import StratifiedShuffleSplit
@@ -87,17 +87,17 @@ def parquet_data_transforamtion(data):
         logger.info(f"----------parquet data transformation".ljust(60, '-'))
         df = data.copy()
         df.drop(['_id'],axis=1, inplace=True)
+        site_df = pd.read_json(r"paired_sensor_site_data.json")
+        df = df.merge(site_df, on='sensor_id', how='left')
+        logger.info(f"columns: {df.columns}")
         # logger.info(f"object type columns:{[col for col in df.columns if df[col].dtype == object]}".ljust(60, '-')) 
-
         # date time handling and index creation
         df["creation_time"] = pd.to_datetime(df['creation_time'])
         df.set_index(['creation_time'],drop= True, inplace= True)
         df.sort_index(inplace= True)
-
-        # sensor_id_list = df['sensor_id'].unique()
+        sensor_id_list = df['sensor_id'].unique()
 
         start_date, end_date = df.first_valid_index(), df.last_valid_index()
-
         # Generate the holiday list
         holiday = holidays_list(start_date, end_date)
         # Convert the holiday list to a pandas DatetimeIndex
@@ -106,15 +106,26 @@ def parquet_data_transforamtion(data):
             df['is_holiday'] = df.index.isin(holiday_dates).astype(int)
         else:
             logger.info("Holiday list generation failed.")
+
         df.reset_index(inplace=True)
-        site_df = pd.read_json(r"paired_sensor_site_data.json")
-        df_merged = df.merge(site_df, on='sensor_id', how='left')
+        # df_merged["creation_time"] = pd.to_datetime(df_merged['creation_time'])
+        # df_merged.set_index(['creation_time'],drop= True, inplace= True)
+        df = label_encoding_decoding(df)
+        df.info()
+        client = MongoClient(os.getenv("mongo_url"),compressors='zstd',zlibCompressionLevel=9)
+        database_name = os.getenv("database_name")
+        collection = get_database(client,database_name)
+        # sensor = db.load_profile_jdvvnl
+        final_df_list = []
 
-        logger.info(f"columns: {df_merged.columns}")
-        df_merged["creation_time"] = pd.to_datetime(df_merged['creation_time'])
-        df_merged.set_index(['creation_time'],drop= True, inplace= True)
-        print(df_merged.info())
-
+        for id, data in df.groupby('label_sensor_id'):
+            transformed_df = data_validation(collection,id,data.copy())
+            if transformed_df is not None:
+                if not transformed_df.empty:
+                    final_df_list.append(transformed_df)
+                    # return transformed_df
+        final_df = pd.concat(final_df_list)
+        return final_df
 
     except Exception as e:
         logger.error(f"error in load profile data transformation:{e}",exc_info=True)
@@ -170,93 +181,3 @@ def image_data_transformation(data):
         logger.error(f"error in image data transformation:{e}",exc_info=True)
 
 ###########################---- ----#########################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import mongo_query.sensor_ids as sensor_ids
-# from config.db_port import get_database
-
-client = MongoClient(os.getenv("mongo_url"),compressors='zstd',zlibCompressionLevel=9)
-database_name = os.getenv("database_name")
-db = get_database(client,database_name)
-sensor = db.load_profile_jdvvnl
-
-def data_fetch(sensor_id, site_id):
-    try:
-        from_id = f"{sensor_id}-2024-01-01 00:00:00"
-        to_id = f"{sensor_id}-2024-03-31 23:59:59"
-        query = {"_id": {"$gte": from_id, "$lt": to_id}}
-
-        results = list(sensor.find(query))
-        for doc in results:
-            if '_id' in doc:
-                doc['_id'] = str(doc['_id'])
-
-        if results:
-            df = datatransformation.init_transformation(results, site_id)
-            if df is None:
-                logger.info(f"Nothing transformed for sensor_id: {sensor_id}")
-            else:
-                logger.info(f"Fetched and transformed {len(df)} records for sensor_id: {sensor_id}")
-            return df
-        else:
-            logger.info(f"No records found for sensor_id: {sensor_id}")
-            return None
-
-    except Exception as e:
-        logger.error(f"Error fetching data for sensor_id {sensor_id}: {e}", exc_info=True)
-        return None
-
-def fetch_data_for_sensors(circle_id, output_dir="sensor_data"):
-    os.makedirs(output_dir, exist_ok=True)
-
-    sensors = sensor_ids.sensor_ids(circle_id)
-    sensorids = [doc["id"] for doc in sensors]
-    site_ids = [doc["site_id"] for doc in sensors]
-
-    all_dicts = []
-    with ThreadPoolExecutor() as executor:
-        futures = {executor.submit(data_fetch, sensor_id, site_id): (sensor_id, site_id) for sensor_id, site_id in zip(sensorids, site_ids)}
-
-        for future in concurrent.futures.as_completed(futures):
-            dicts = future.result()
-            if dicts is not None and len(dicts) > 0:
-                all_dicts.extend(dicts)
-
-    if all_dicts:
-        combined_df = pd.DataFrame(all_dicts)
-        
-        # Convert all object type columns to strings if necessary
-        combined_df = combined_df.applymap(lambda x: str(x) if isinstance(x, ObjectId) else x)
-
-        table = pa.Table.from_pandas(combined_df)
-        pq.write_table(table, os.path.join(output_dir, f"{circle_id}_data.parquet"))
-        
-        logger.info(f"Saved data for circle_id: {circle_id}")
-        return "saved"
-    else:
-        logger.info(f"No data to save for circle_id: {circle_id}")
-        return "no data"
-
